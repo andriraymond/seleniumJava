6ee428581a5c596c76f3cc6be3f2229920666701
andriraymond <andriraymondo@gmail.com>
2024-08-16 20:25:19 +0700

    feat(#1) : Add saucedemo login test case

---
+++ src/test/java/saucedemo/LoginTest.java
@@ -0,0 +1,27 @@
+package saucedemo;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @Test
+    public void login() {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\App\\driver\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        // Go to saucedemo login page
+        driver.navigate().to("https://www.saucedemo.com");
+        driver.manage().window().maximize();
+
+        // Fill username and password
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+}
